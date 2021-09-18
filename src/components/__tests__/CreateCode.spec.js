80d008cb69ef970825f16ee49c2a335f1365ae15
Kentarou Asai <asaknkn@gmail.com>
2021-09-18 11:49:27 +0900

    fix: CreateCode test

--- src/components/__tests__/CreateCode.spec.js
+++ src/components/__tests__/CreateCode.spec.js
@@ -6,20 +6,43 @@ import axios from "axios";
 jest.mock("axios");
 var mockedAxios = axios;
 describe('CreateCode.vue', function () {
-    it('table head', function () {
+    it('input table has expeteced colums', function () {
         var wrapper = mount(CreateCode);
         var tableHeadRows = wrapper.findAll('table > thead > tr > th');
+        var tableBodyRows = wrapper.findAll('table > tbody > tr');
         expect(tableHeadRows.at(0).text()).toBe('Paramenter');
         expect(tableHeadRows.at(1).text()).toBe('Value');
+        expect(tableBodyRows.length).toBe(18);
     });
-    it('table body', function () {
-        var wrapper = mount(CreateCode);
-        var tableHeadRows = wrapper.findAll('table > tbody > tr');
-        expect(tableHeadRows.length).toBe(3);
-        expect(tableHeadRows.at(0).text()).toBe('merchantPaymentId');
-        expect(tableHeadRows.at(1).text()).toBe('amount');
-        expect(tableHeadRows.at(2).text()).toBe('currency');
-    });
+    it('output table does not exist', function () { return __awaiter(void 0, void 0, void 0, function () {
+        var wrapper, tables;
+        return __generator(this, function (_a) {
+            wrapper = mount(CreateCode);
+            tables = wrapper.findAll('table');
+            expect(tables.length).toBe(1);
+            return [2 /*return*/];
+        });
+    }); });
+    it('output table  exists', function () { return __awaiter(void 0, void 0, void 0, function () {
+        var wrapper, tables;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    wrapper = mount(CreateCode);
+                    return [4 /*yield*/, wrapper.setData({ response: {
+                                resultInfo: {
+                                    code: "sample"
+                                }
+                            } })];
+                case 1:
+                    _a.sent();
+                    tables = wrapper.findAll('table');
+                    expect(wrapper.vm.$data.response.resultInfo.code).toBe("sample");
+                    expect(tables.at(1).exists()).toBe(true);
+                    return [2 /*return*/];
+            }
+        });
+    }); });
     it('when triggers a click, response success ', function () { return __awaiter(void 0, void 0, void 0, function () {
         var wrapper, response;
         return __generator(this, function (_a) {
@@ -66,7 +89,67 @@ describe('CreateCode.vue', function () {
                             authorizationExpiry: 0
                         }
                     };
-                    mockedAxios.post.mockResolvedValueOnce({ data: JSON.stringify(response) });
+                    jest.spyOn(window, 'confirm').mockReturnValue(true);
+                    jest.spyOn(window, 'open').mockReturnValue(null);
+                    mockedAxios.post.mockResolvedValueOnce({ data: response });
+                    return [4 /*yield*/, wrapper.find('button').trigger('click')];
+                case 1:
+                    _a.sent();
+                    return [4 /*yield*/, flushPromises()];
+                case 2:
+                    _a.sent();
+                    expect(wrapper.vm.$data.response).toEqual(response);
+                    return [2 /*return*/];
+            }
+        });
+    }); });
+    it('when triggers a click, response failure ', function () { return __awaiter(void 0, void 0, void 0, function () {
+        var wrapper, response;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    wrapper = mount(CreateCode);
+                    response = {
+                        resultInfo: {
+                            code: "string",
+                            message: "string",
+                            codeId: "string"
+                        },
+                        data: {
+                            codeId: "string",
+                            url: "string",
+                            deeplink: "string",
+                            expiryDate: 0,
+                            merchantPaymentId: "string",
+                            amount: {
+                                amount: 0,
+                                currency: "JPY"
+                            },
+                            orderDescription: "string",
+                            orderItems: [
+                                {
+                                    name: "string",
+                                    category: "string",
+                                    quantity: 1,
+                                    productId: "string",
+                                    unit_price: {
+                                        amount: 0,
+                                        currency: "JPY"
+                                    }
+                                }
+                            ],
+                            codeType: "string",
+                            storeInfo: "string",
+                            storeId: "string",
+                            terminalId: "string",
+                            requestedAt: 0,
+                            redirectUrl: "string",
+                            redirectType: "WEB_LINK",
+                            isAuthorization: false,
+                            authorizationExpiry: 0
+                        }
+                    };
+                    mockedAxios.post.mockResolvedValueOnce({ data: response });
                     return [4 /*yield*/, wrapper.find('button').trigger('click')];
                 case 1:
                     _a.sent();
--- src/components/__tests__/CreateCode.spec.ts
+++ src/components/__tests__/CreateCode.spec.ts
@@ -1,4 +1,4 @@
-import { mount, Wrapper } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import CreateCode from '@/components/CreateCode.vue'
 import {response} from '@/common/interface/createCode'
 import flushPromises from "flush-promises"
@@ -8,22 +8,32 @@ jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
 describe('CreateCode.vue', () => {
-  it('table head', () => {
+  it('input table has expeteced colums', () => {
     const wrapper = mount(CreateCode)
     const tableHeadRows = wrapper.findAll('table > thead > tr > th')
+    const tableBodyRows = wrapper.findAll('table > tbody > tr')
 
     expect(tableHeadRows.at(0).text()).toBe('Paramenter')
     expect(tableHeadRows.at(1).text()).toBe('Value')
+    expect(tableBodyRows.length).toBe(18);
   })
 
-  it('table body', () => {
+  it('output table does not exist', async () => {
     const wrapper = mount(CreateCode)
-    const tableHeadRows = wrapper.findAll('table > tbody > tr')
+    const tables = wrapper.findAll('table')
+    expect(tables.length).toBe(1);
+  })
 
-    expect(tableHeadRows.length).toBe(3);
-    expect(tableHeadRows.at(0).text()).toBe('merchantPaymentId')
-    expect(tableHeadRows.at(1).text()).toBe('amount')
-    expect(tableHeadRows.at(2).text()).toBe('currency')
+  it('output table  exists', async () => {
+    const wrapper = mount(CreateCode)
+    await wrapper.setData({ response: {
+      resultInfo: {
+        code: "sample"
+      }
+    } })
+    const tables = wrapper.findAll('table')
+    expect(wrapper.vm.$data.response.resultInfo.code).toBe("sample")
+    expect(tables.at(1).exists()).toBe(true);
   })
 
   it('when triggers a click, response success ', async () => {
@@ -69,7 +79,59 @@ describe('CreateCode.vue', () => {
         authorizationExpiry: 0
       }
     }
-    mockedAxios.post.mockResolvedValueOnce({data: JSON.stringify(response)})
+    jest.spyOn(window, 'confirm').mockReturnValue(true)
+    jest.spyOn(window, 'open').mockReturnValue(null)
+    mockedAxios.post.mockResolvedValueOnce({data: response})
+    await wrapper.find('button').trigger('click')
+    await flushPromises()
+
+    expect(wrapper.vm.$data.response).toEqual(response)
+  })
+
+  it('when triggers a click, response failure ', async () => {
+    const wrapper = mount(CreateCode)
+
+    const response: response = {
+      resultInfo: {
+        code: "string",
+        message: "string",
+        codeId: "string"
+      },
+      data: {
+        codeId: "string",
+        url: "string",
+        deeplink: "string",
+        expiryDate: 0,
+        merchantPaymentId: "string",
+        amount: {
+          amount: 0,
+          currency: "JPY"
+        },
+        orderDescription: "string",
+        orderItems: [
+          {
+            name: "string",
+            category: "string",
+            quantity: 1,
+            productId: "string",
+            unit_price: {
+              amount: 0,
+              currency: "JPY"
+            }
+          }
+        ],
+        codeType: "string",
+        storeInfo: "string",
+        storeId: "string",
+        terminalId: "string",
+        requestedAt: 0,
+        redirectUrl: "string",
+        redirectType: "WEB_LINK",
+        isAuthorization: false,
+        authorizationExpiry: 0
+      }
+    }
+    mockedAxios.post.mockResolvedValueOnce({data: response})
     await wrapper.find('button').trigger('click')
     await flushPromises()
 
